66838ba80226f5f4960eceaed3a1d2cb48e23e09
Chayan Hazra <chayan@2pirad.com>
2026-01-08 02:04:15 +0530

    environment setup

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def main():
+    print(os.getenv("OPENAI_API_KEY"))
+    print("Hello from langchain-course!")
+
+
+if __name__ == "__main__":
+    main()
